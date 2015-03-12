d5b823c2e4c165f1044397e67cba7b9af51948b8
jyroman <jyroman@hotmail.fr>
2015-03-12 12:43:24 +0100

    Finalisation et amélioration du fichier UIDfind.sh - Recherche UID apache2 : OK - Recherche UID mysqld : OK

--- UIDfind.sh
+++ UIDfind.sh
@@ -2,6 +2,10 @@
 
 #Trouver le UID du process apache2
 
-pida=$(ps -ef | grep apache2 | cut -d ' ' -f 1 | uniq -d)
-echo $pida
+# recherche de apache2 dans les processus et séparation des colonnes
+pida=$(ps -ef | grep apache2 | cut -d ' ' -f 1 | sed "/$USER/d" | uniq)
+echo http_user_pid : $pida
 
+# recherche de mysqld dans les processus et séparation des colonnes
+pidm=$(ps -ef | grep mysqld | cut -d ' ' -f 1 | sed "/$USER/d")
+echo mysql_user_pid : $pidm
