faf7ae5c48304b7a014c67b26adf81e2bf5d60f7
jyroman <jyroman@hotmail.fr>
2015-03-19 09:40:27 +0100

    Finalisation du fichier UIDfind.sh Cas d'erreur quand les processus ne sont pas trouvé : OK Récupération des UID : OK

--- UIDfind.sh
+++ UIDfind.sh
@@ -4,8 +4,20 @@
 
 # recherche de apache2 dans les processus et séparation des colonnes
 pida=$(ps -ef | grep apache2 | cut -d ' ' -f 1 | sed "/$USER/d" | uniq)
-echo http_user_pid : $pida
+# test de la variable si le processus a été trouvé
+if [ -z $pida ];
+then
+	echo "erreur le processus apache2 n'a pas été trouvé"
+else 
+	echo http_user_pid : $pida
+fi
 
 # recherche de mysqld dans les processus et séparation des colonnes
 pidm=$(ps -ef | grep mysqld | cut -d ' ' -f 1 | sed "/$USER/d")
-echo mysql_user_pid : $pidm
+# test de la variable si le processus a été trouvé
+if [ -z $pidm ];
+then
+	echo "erreur le processus mysqld n'as pas été trouvé"
+else
+	echo mysql_user_pid : $pidm
+fi
