b278984970f7fe68db985259ce0a2102f5fb9636
jyroman <jyroman@hotmail.fr>
2015-03-19 11:47:13 +0100

    en cour de modif

--- pmainstall.sh
+++ pmainstall.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo instllation de phpmyadmin
-echo "phpmyadmin phpmyadmin/reconfigure-webserver apache2" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/db/app-user maggie" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/method "unix socket""| debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/admin-user root" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/method select "unix socket""| debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-user string root" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/dbconfig-install true" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/app-password-confirm maggie" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/admin-pass glenn" | debconf-set-selections
