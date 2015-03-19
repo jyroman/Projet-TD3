218db02005d572ca3afcbc819015eafd0522e53a
jyroman <jyroman@hotmail.fr>
2015-03-19 15:30:08 +0100

    Nouvelle modification des lignes pour pma

--- pmainstall.sh
+++ pmainstall.sh
@@ -4,12 +4,12 @@ echo instllation de phpmyadmin
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/method select "unix socket""| debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/admin-user string root" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/dbconfig-install true" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/app-password-confirm maggie" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/admin-pass glenn" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/password-confirm maggie" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/setup-password maggie" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/app-pass glenn" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password maggie" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password glenn" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/password-confirm password maggie" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/setup-password password maggie" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password glenn" | debconf-set-selections
 apt-get update
 apt-get install -y phpmyadmin
 echo "Fin de l'installation de phpmyadmin"
