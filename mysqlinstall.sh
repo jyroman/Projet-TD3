20473abc3fce11032ab9826bd716a7fdc79ab39a
jyroman <jyroman@hotmail.fr>
2015-03-19 11:48:53 +0100

    Update mysqlinstall.sh
    correction d'une erreur

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -3,6 +3,6 @@
 echo installation de mysql-server
 echo « mysql-server mysql-server/root_password password glenn » | debconf-set-selections
 echo « mysql-server mysql-server/root_password_again password glenn » | debconf-set-selections
-apt_get updates
+apt-get updates
 apt-get install -y mysql-server
 echo "fin de l'installation de mysql"
