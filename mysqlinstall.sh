ab9377cdb3cbbe7a9fe01663433ce4c3c3a87fc4
jyroman <jyroman@hotmail.fr>
2015-03-19 10:24:41 +0100

    rectification d'une erreur ne permettant pas l'installation de phpmyadmin

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo installation de mysql-server
-echo « mysql-server mysql-server/root_password password glenn » | debconf-set-selections
-echo « mysql-server mysql-server/root_password_again password glenn » | debconf-set-selections
+echo "mysql-server mysql-server/root_password password glenn" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password glenn" | debconf-set-selections
 apt-get updates
 apt-get install -y mysql-server
 echo "fin de l'installation de mysql"
